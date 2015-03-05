c0c13686751ecf2b7634f9167744ecf89cfe660d
Sharon Yang <sharonyang@caltech.edu>
2015-03-05 05:37:34 -0800

    First commit

---
+++ voiceParseHelpers/InsertionClass.h
@@ -0,0 +1,13 @@
+//
+//  InsertionClass.h
+//  voiceParseHelpers
+//
+//  Created by Sharon Yang on 3/5/15.
+//  Copyright (c) 2015 Sharon Yang. All rights reserved.
+//
+
+#ifndef voiceParseHelpers_InsertionClass_h
+#define voiceParseHelpers_InsertionClass_h
+
+
+#endif
---
+++ voiceParseHelpers/LocationClass.h
@@ -0,0 +1,13 @@
+//
+//  LocationClass.h
+//  voiceParseHelpers
+//
+//  Created by Sharon Yang on 3/5/15.
+//  Copyright (c) 2015 Sharon Yang. All rights reserved.
+//
+
+#ifndef voiceParseHelpers_LocationClass_h
+#define voiceParseHelpers_LocationClass_h
+
+
+#endif
