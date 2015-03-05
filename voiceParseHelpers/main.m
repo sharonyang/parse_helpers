0f0978a2e528bde8cb2e8069665396487cfba827
Sharon Yang <sharonyang@caltech.edu>
2015-03-05 05:28:28 -0800

    Initial Commit

---
+++ voiceParseHelpers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  voiceParseHelpers
+//
+//  Created by Sharon Yang on 3/5/15.
+//  Copyright (c) 2015 Sharon Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
